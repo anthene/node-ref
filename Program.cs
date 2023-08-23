bba69fb39be80f3b3d71da6e0f3772ed59342f6f
Anton Medvedev <Anton.Medvedev@capgroup.com>
2023-08-23 16:20:24 -0700

    ASP.NET static files (wwwroot/index.html)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/hello", () => "Hello World!");
 
+app.UseDefaultFiles(); // must be called before UseStaticFiles to serve the default file.
+app.UseStaticFiles();
 app.Run();
