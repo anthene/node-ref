9ee266395cb8d74c06f4fddd261b963b1bdfcaaf
Anton Medvedev <Anton.Medvedev@capgroup.com>
2023-09-06 13:01:27 -0700

    HTTPS redirection. 500 HTTP response code endpoint

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Urls.Add("http://localhost:5000");
+app.Urls.Add("https://localhost:5001");
 app.MapGet("/api/hello", () => "Hello World!");
+app.MapGet("/api/not-impl", () => { throw new NotImplementedException(); });
 
+app.UseHttpsRedirection();
 app.UseDefaultFiles(); // must be called before UseStaticFiles to serve the default file.
 app.UseStaticFiles();
 app.Run();
